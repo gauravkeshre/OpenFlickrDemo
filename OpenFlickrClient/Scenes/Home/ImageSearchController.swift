a78ee907886686d587c7000a08d63b075ac5d182
Gaurav <gaurav.keshre@gmail.com>
2018-07-02 01:42:11 +0530

    added HUD and pagination

--- OpenFlickrClient/Scenes/Home/ImageSearchController.swift
+++ OpenFlickrClient/Scenes/Home/ImageSearchController.swift
@@ -12,10 +12,18 @@ struct Search: Equatable {
     let keyword: String
     var pageNumber: Int = -1
     
+    @discardableResult
+    mutating func incrementPage() -> Search {
+        pageNumber = pageNumber + 1
+        return self
+    }
+    
+    /** Equatable */
     static func == (lhs: Search, rhs: Search) -> Bool {
         return lhs.keyword == rhs.keyword
     }
 }
+
 
 final class ImageSearchController: UIViewController {
     
@@ -23,7 +31,7 @@ final class ImageSearchController: UIViewController {
     @IBOutlet private weak var collectionView : UICollectionView!
     @IBOutlet private weak var txtSearch : UITextField!
     @IBOutlet private weak var btnGo : UIButton!
-
+    
     
     //MARK:- Private Props
     /** Keeping track of the presently running `NSURLSessionTask`. We will need to cancel the presnet task as the user types and enters */
@@ -36,8 +44,15 @@ final class ImageSearchController: UIViewController {
     private lazy var cellSize: CGSize = {
         return self.collectionView.itemSize(numberOfItemsPerRow: Constants.CellsPerRow, paddingBetweenItems: Constants.PaddingBetweenItems)
     }()
-    //MARK:- Public Props
     
+    private lazy var headerSize: CGSize = {
+        var size = collectionView.bounds.size
+        size.width = size.width - CGFloat( 2 * Constants.PaddingBetweenItems)
+        size.height = 110
+        return size
+    }()
+    
+
     
     //MARK:- LifeCycle Method
     override func viewDidLoad() {
@@ -53,54 +68,76 @@ final class ImageSearchController: UIViewController {
         /** Fetch Intial images with "popular tag" */
         txtSearch.text = "popular"
         triggerSearch(Search(keyword: "popular", pageNumber: 1))
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 }
 
 /** Search */
-extension ImageSearchController {
+private extension ImageSearchController {
     
-    func triggerSearch(_ search: Search) {
+    func triggerSearch(_ search: Search, showHUD: Bool = true) {
+        func toggleHUD(visible: Bool = true) {
+            guard showHUD else { return }
+            DispatchQueue.main.async {
+                
+                if visible {
+                    HUD.show(on: self)
+                }else {
+                    HUD.hide()
+                }
+            }
+        }
         
+        /** Check if this is just a page number update or a new keyword.
+                if this is a new keyword, we will not append new results in existing array but just refresh the array
+         */
         let refresh = search != currentSearch
-        
         if refresh {
-            currentSearch = search
-        }else {
-            currentSearch.pageNumber += 1
+            self.currentSearch = search
         }
         
         task?.cancelTask()
-        HUD.show(on: self)
+        toggleHUD()
         task = FlickrService.fetchFlickrPhoto(tag: search.keyword, page: search.pageNumber) { (result) in
             switch result {
             case .success( let response):
                 guard let photos = response.body?.photos else {
                     // Error Handling here.
+                    toggleHUD(visible: false)
                     return
                 }
+                
+                /** Increment only if the request was successfull */
+                self.currentSearch.incrementPage()
+                
+                /** If this was a fresh search. Just clean existing data source array */
                 if refresh {
                     self.arrPhotos.removeAll()
                 }
+                
+                /** Insert new indexPaths in the collectionview. that offsets from `arrPhotos.count` till new  `arrPhotos.count + photos. count -1` */
+                let startIndex = self.arrPhotos.count
                 self.arrPhotos.append(contentsOf: photos)
+                let endIndex = self.arrPhotos.count - 1
+                
+                toggleHUD(visible: false)
                 DispatchQueue.main.async {
                     // to be improved
-                    self.collectionView.reloadData()
-                    HUD.hide()
+                    
+                    self.collectionView.performBatchUpdates({
+                        let indexPaths = Array(startIndex...endIndex).map { IndexPath(item: $0, section: 1) }
+                        self.collectionView.insertItems(at: indexPaths)
+                    }, completion: nil)
                 }
+                
             case .failure (let err):
                 print("error: \(err.message)")
-                HUD.hide()
+                toggleHUD(visible: false)
             }
         }
     }
 }
 /** Actions */
-extension ImageSearchController {
+private extension ImageSearchController {
     @IBAction
     func handleTap(_ sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
@@ -112,64 +149,63 @@ extension ImageSearchController {
             return
         }
         triggerSearch(Search(keyword: text, pageNumber: 1))
-        
     }
 }
 
-/** UITextField */
+/** UITextField Protocol conformation */
 extension ImageSearchController: UITextFieldDelegate {
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        
         guard let keyword = textField.text, keyword.count >= 2 else {
             return true
         }
-        
         triggerSearch(Search(keyword: keyword, pageNumber: 1))
-        
         return true
     }
 }
 
-/** UICollectionView */
+/** UICollectionView Protocol conformation */
 extension ImageSearchController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    
-    
     //MARK:- UICollectionViewDataSource
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       
+        
         return section == 0 ? 1 : arrPhotos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
+        /** Header Cell */
         if indexPath.section == 0 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeaderCell", for: indexPath)
             return cell
         }
         
+        /** Image Cell */
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCell
         cell.configure(arrPhotos[indexPath.item])
         return cell
     }
     
     
+    func collectionView(_ collectionView: UICollectionView,
+                        willDisplay cell: UICollectionViewCell,
+                        forItemAt indexPath: IndexPath) {
+        
+        guard arrPhotos.count > 0 else  { return }
+        guard indexPath.section == 1 else { return }
+        
+        if indexPath.row >= arrPhotos.count -  (  3 * Constants.CellsPerRow) - 1 {
+            triggerSearch(currentSearch, showHUD: false)
+        }
+    }
+    
     //MARK:- UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        if indexPath.section == 0 {
-            var size = collectionView.bounds.size
-            size.width = size.width - CGFloat( 2 * Constants.PaddingBetweenItems)
-            size.height = 110
-            return size
-        }
-        return cellSize
+        return indexPath.section == 0 ? headerSize : cellSize
     }
     
     
